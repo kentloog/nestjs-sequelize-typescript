d7fbfa2e684b23b82dabd1764fc9e3abca5c59ec
Kent Loog <kentloog@gmail.com>
2019-04-24 17:56:02 +0300

    inject usersRepository into usersService and implement it in registering user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,30 @@
 import { CreateUserDto } from './dto/create-user.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+    constructor(
+        @Inject('UsersRepository')
+        private readonly usersRepository: typeof User,
+    ) {}
+
     private readonly users: CreateUserDto[] = [];
 
-    register(user: CreateUserDto) {
-        this.users.push(user);
-        return user;
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const user = new User();
+        user.email = createUserDto.email.trim().toLowerCase();
+        user.password = createUserDto.password;
+        user.firstName = createUserDto.firstName;
+        user.lastName = createUserDto.lastName;
+        user.gender = createUserDto.gender;
+        user.birthday = createUserDto.birthday;
+
+        try {
+            return await user.save();
+        } catch (err) {
+            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     findAll(): CreateUserDto[] {
