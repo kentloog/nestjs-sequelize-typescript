90c9b52f13c23942abfedda2a1cef033147df0db
Kent Loog <kentloog@gmail.com>
2019-04-24 17:56:19 +0300

    update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
-    register(@Body() createUserDto: CreateUserDto): CreateUserDto {
-        return this.usersService.register(createUserDto);
+    register(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.usersService.create(createUserDto);
     }
 
     @Get()
