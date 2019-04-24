ed2cd36a1864617449806425b3e3a22e7e838fad
Kent Loog <kentloog@gmail.com>
2019-04-24 16:52:22 +0300

    add dto for creating users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,30 @@
+import {
+    IsString,
+    IsEmail,
+    IsEnum,
+    IsISO8601,
+    IsOptional,
+} from 'class-validator';
+import { Gender } from './../shared/common/enum/gender';
+
+export class CreateUserDto {
+    @IsEmail()
+    readonly email: string;
+
+    @IsString()
+    readonly password: string;
+
+    @IsString()
+    readonly firstName: string;
+
+    @IsString()
+    readonly lastName: string;
+
+    @IsOptional()
+    @IsEnum(Gender)
+    readonly gender: Gender;
+
+    @IsOptional()
+    @IsISO8601()
+    readonly birthday: string;
+}
---
+++ src/users/shared/common/enum/gender.ts
@@ -0,0 +1,4 @@
+export enum Gender {
+    female = 'female',
+    male = 'male',
+}
