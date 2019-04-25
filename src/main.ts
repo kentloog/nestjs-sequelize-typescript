49cfdff987ca5096f90bfe60cc610267349f9eab
Kent Loog <kentloog@gmail.com>
2019-04-25 10:43:09 +0300

    initialize swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    setupSwagger(app);
     await app.listen(3000);
 }
+
 bootstrap();
---
+++ src/swagger.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+    const options = new DocumentBuilder()
+        .setTitle('Nest.js example API')
+        .setDescription('API Documentation')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('documentation', app, document);
+}
