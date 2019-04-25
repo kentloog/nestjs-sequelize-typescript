c050f9665d1167914781538f21a7737a299281e9
Kent Loog <kentloog@gmail.com>
2019-04-25 15:34:31 +0300

    create interface for sequelize config

---
+++ src/shared/config/interfaces/sequelize-orm-config.interface.ts
@@ -0,0 +1,10 @@
+export interface SequelizeOrmConfig {
+    dialect: string;
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    database: string;
+    logging?: boolean;
+    operatorsAliases: boolean;
+}
