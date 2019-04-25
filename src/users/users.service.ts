959a595a44e6393aa30adbaf971a78510f73adb1
Kent Loog <kentloog@gmail.com>
2019-04-25 12:29:18 +0300

    create login method in usersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,31 @@
-import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { User } from './user.entity';
-import { genSalt, hash } from 'bcrypt';
+import { genSalt, hash, compare } from 'bcrypt';
 import { UserDto } from './dto/user.dto';
+import { UserLoginRequestDto } from './dto/user-login-request.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthService } from './../shared/auth/auth.service';
 
 @Injectable()
 export class UsersService {
     constructor(
         @Inject('UsersRepository')
         private readonly usersRepository: typeof User,
+        private readonly authService: AuthService,
     ) {}
+
+    async findAll(): Promise<UserDto[]> {
+        const users = await this.usersRepository.findAll<User>();
+        return users.map(user => {
+            return new UserDto(user);
+        });
+    }
+
+    async getUserByEmail(email: string): Promise<User> {
+        return await this.usersRepository.findOne<User>({
+            where: { email },
+        });
+    }
 
     async create(createUserDto: CreateUserDto): Promise<UserDto> {
         try {
@@ -37,16 +53,26 @@ export class UsersService {
         }
     }
 
-    async findAll(): Promise<UserDto[]> {
-        const users = await this.usersRepository.findAll<User>();
-        return users.map(user => {
-            return new UserDto(user);
-        });
-    }
+    async login(userLoginRequestDto: UserLoginRequestDto): Promise<string> {
+        const email = userLoginRequestDto.email;
+        const password = userLoginRequestDto.password;
 
-    async getUserByEmail(email: string): Promise<User> {
-        return await this.usersRepository.findOne<User>({
-            where: { email },
-        });
+        const user = await this.getUserByEmail(email);
+        if (!user) {
+            throw new HttpException(
+                'Invalid email or password.',
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+
+        const isMatch = await compare(password, user.password);
+        if (!isMatch) {
+            throw new HttpException(
+                'Invalid email or password.',
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+
+        return await this.authService.signToken(user);
     }
 }
