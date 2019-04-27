735afdd632a62c317249c26a2e5d51cd5d8ce5fc
Kent Loog <kentloog@gmail.com>
2019-04-27 23:30:45 +0300

    set up testing module

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Sequelize } from 'sequelize-typescript';
+import { ConfigService } from 'src/shared/config/config.service';
+import { Post } from './../src/posts/post.entity';
+import { User } from './../src/users/user.entity';
 
 describe('/', () => {
     let app: INestApplication;
     let sequelize: Sequelize;
+
+    beforeAll(async () => {
+        const module = await Test.createTestingModule({
+            imports: [AppModule],
+            providers: [
+                {
+                    provide: 'SEQUELIZE',
+                    useFactory: (configService: ConfigService) => {
+                        sequelize = new Sequelize(
+                            configService.sequelizeOrmConfig,
+                        );
+
+                        sequelize.addModels([User, Post]);
+
+                        return sequelize;
+                    },
+                    inject: [ConfigService],
+                },
+            ],
+        }).compile();
+
+        app = module.createNestApplication();
+        app.useGlobalPipes(new ValidationPipe());
+        await app.init();
+    });
 });
